da81a6cd19388d4f37b6eae35177b5aeed0f4298
Jorge Lillo <jorge.lillo9@gmail.com>
2015-06-11 23:13:04 +0200

    Añadido template con tabs, login contra api rest hecho

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # top_games_ionic
-Proyecto fin de m√°ster 2014-2015 (M√°ster en Ingenier√≠a Web), Universidad de Polit√©cnica de Madrid, Jorge Lillo Cobacho
+Proyecto fin de m·ster 2014-2015 (M·ster en IngenierÌa Web), Universidad de PolitÈcnica de Madrid, Jorge Lillo Cobacho
---
+++ www/js/services.js
@@ -0,0 +1,87 @@
+var SERVER_URL = 'http://127.0.0.1:8000/api/';
+angular.module('starter.services', [])
+ 
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  },{
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.service('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw, http) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+			http.defaults.headers.post['Access-Control-Allow-Credentials'] = 'true';
+			http.post(SERVER_URL + 'users/login', {
+                user: name,
+                pass: pw
+            }, { withCredentials: true }).success(function(data) {
+					 if(data.login == true){
+						 deferred.resolve('Welcome ' + name + '!');
+					 } else {
+						 deferred.reject('Wrong credentials.');
+					 }
+					 
+				})
+				.error(function(data) {
+					 deferred.reject('Wrong credentials.');
+				});
+			
+            
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+});
